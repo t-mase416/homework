151fb7ebac235b3af654322ea94b765b0a0b3e75
Takayuki Mase <tmase@c1r7s1.42tokyo.jp>
2025-07-24 17:02:42 +0900

    7/24

--- gnl/get_next_line.c
+++ gnl/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: tmase <tmase@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/29 21:00:13 by tmase             #+#    #+#             */
-/*   Updated: 2025/06/06 22:01:50 by tmase            ###   ########.fr       */
+/*   Updated: 2025/07/24 15:40:53 by tmase            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -106,19 +106,20 @@ char	*get_next_line(int fd)
 	return (line);
 }
 
-// #include <fcntl.h>
+#include <fcntl.h>
+#include <stdio.h>
 
-// int	main(void)
-// {
-// 	int fd;
-// 	char *line;
+int	main(void)
+{
+	int fd;
+	char *line;
 
-// 	fd = open("test.txt", O_RDONLY);
-// 	while ((line = get_next_line(fd)) != NULL)
-// 	{
-//     	printf("%s\n", line);
-//    		free(line);
-// 	}
-// 	close(fd);
-// 	return (0);
-// }
+	fd = open("test.txt", O_RDONLY);
+	while ((line = get_next_line(fd)) != NULL)
+	{
+    	printf("%s\n", line);
+   		free(line);
+	}
+	close(fd);
+	return (0);
+}
---
+++ test/get_next_line.h
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tmase <tmase@student.42tokyo.jp>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/29 21:00:10 by tmase             #+#    #+#             */
+/*   Updated: 2025/07/24 16:58:12 by tmase            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 100
+# endif
+
+# include <stdlib.h>
+# include <unistd.h>
+# include <stdio.h>
+# include <stdint.h>
+
+char	*get_next_line(int fd);
+
+#endif
