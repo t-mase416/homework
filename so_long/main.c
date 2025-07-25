c3660e921e4d0ce9be4f331af9c2c3f68171ca38
Takayuki Mase <tmase@c2r1s1.42tokyo.jp>
2025-07-25 22:13:18 +0900

    7/25-描画成功

--- so_long/main.c
+++ so_long/main.c
@@ -6,7 +6,7 @@
 /*   By: tmase <tmase@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/12 15:26:18 by tmase             #+#    #+#             */
-/*   Updated: 2025/07/18 16:40:00 by tmase            ###   ########.fr       */
+/*   Updated: 2025/07/25 21:59:36 by tmase            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,20 +18,50 @@
 #include "so_long.h"
 #include "map/map.h"
 
-int	close_by_cross(t_game *game)
+// int	close_by_cross(t_game *game)
+// {
+// 	mlx_destroy_window(game->mlx, game->win);
+// 	exit(0);
+// 	return (0);
+// }
+
+// int	close_by_esc(int keycode, t_game *game)
+// {
+// 	if (keycode == KEY_ESC)
+// 	{
+// 		mlx_destroy_window(game->mlx, game->win);
+// 		exit(0);
+// 	}
+// 	return (0);
+// }
+
+int	cleanup_and_exit(t_game *game)
 {
-	mlx_destroy_window(game->mlx, game->win);
+	if (game->map)
+		free_map(game->map);
+	destroy_images(game);
+	if (game->win)
+		mlx_destroy_window(game->mlx, game->win);
+	if (game->mlx)
+	{
+		mlx_destroy_display(game->mlx);
+		free(game->mlx);
+	}
+	printf("Game closed\n");
 	exit(0);
 	return (0);
 }
 
-int	close_by_esc(int keycode, t_game *game)
+int	handle_keypress(int keycode, t_game *game)
 {
 	if (keycode == KEY_ESC)
-	{
-		mlx_destroy_window(game->mlx, game->win);
-		exit(0);
-	}
+		cleanup_and_exit(game);
+	return (0);
+}
+
+int	handle_destroy(t_game *game)
+{
+	cleanup_and_exit(game);
 	return (0);
 }
 
@@ -54,20 +84,23 @@ int main(int argc, char **argv)
 		return (1);
 	}
 	game.mlx = mlx_init();
-	game.map_height = get_map_size(game.map) * TILE_SIZE;
-	game.map_width = ft_strlen(game.map[0]) * TILE_SIZE;
-	game.win = mlx_new_window(game.mlx, game.map_height, game.map_width, "So Long");
+	game.map_height = get_map_size(game.map);
+	game.map_width = ft_strlen(game.map[0]);
+	game.win = mlx_new_window(game.mlx, game.map_width * TILE_SIZE,
+		game.map_height * TILE_SIZE, "So Long");
 	if (!game.win)
 	{
 		printf("Error: mlx_new_window() failed\n");
 		if (game.map)
 			free_map(game.map);
+		mlx_destroy_display(game.mlx);
+		free(game.mlx);
 		return (1);
 	}
 	load_images(&game);
 	draw_map(&game);
-	mlx_hook(game.win, 17, (1L<<17), close_by_cross, &game);
-	mlx_hook(game.win, 2, (1L<<0), close_by_esc, &game);
+	mlx_hook(game.win, 17, 0, handle_destroy, &game);
+	mlx_hook(game.win, 2, (1L<<0), handle_keypress, &game);
 	mlx_loop(game.mlx);
 	return (0);
 }
